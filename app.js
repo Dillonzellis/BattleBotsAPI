f84a0e6f2a4a194beeabd9949fdc0117aef013e1
Dillon Ellis <dillonzellis@gmail.com>
2022-02-18 10:47:14 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
+const fs = require("fs");
+const writeStream = fs.createWriteStream("output.csv");
+
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+const url = "https://battlebots.fandom.com/wiki/Discovery_Season_6";
+
+const peopleArr = ["joe", "bob", "sue", "cody"];
+
+axios
+  .get(url)
+  .then((res) => {
+    const $ = cheerio.load(res.data);
+    $(".article-table").each((index, element) => {
+      const botTitles = $(element).find("td").text();
+      console.log(botTitles);
+
+      let botArr = botTitles.split(" ");
+      let cleanArr = botArr.filter((a) => a !== "vs.");
+      console.log(`${cleanArr}`);
+
+      writeStream.write(`Bot name: ${cleanArr} \n`);
+    });
+  })
+  .catch((err) => console.error(err));
+
+app.listen(port, () => {
+  console.log(`Server Established and running on Port ${port}`);
+});
