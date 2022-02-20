85019c32a92d2d543f41beefeef4d0ed9d789861
Dillon Ellis <dillonzellis@gmail.com>
2022-02-20 14:03:01 -0500

    Add bot matchups for individual episodes

--- app.js
+++ app.js
@@ -13,7 +13,23 @@ async function start() {
     );
   });
   const slicedTitles = episodesTitles.slice(2, 12);
-  await fs.writeFile("test.txt", slicedTitles.join("\r\n"));
+  // await fs.writeFile("outputs.txt", slicedTitles.join("\r\n"));
+
+  // table selector range
+  // "#mw-content-text > div > table:nth-child(37) a"
+  // "#mw-content-text > div > table:nth-child(n+37):nth-child(-n+55) a"
+
+  //bot match ups
+  const botMatch1 = await page.evaluate(() => {
+    return Array.from(
+      document.querySelectorAll(
+        "#mw-content-text > div > table:nth-child(37) a"
+      )
+    ).map((x) => x.textContent);
+  });
+
+  console.log(slicedTitles[0]);
+  console.log(botMatch1);
 
   await browser.close();
 }
