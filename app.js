be331fbeb3191b39177bbb96936a205a42773626
Cody Ruggiero <codyruggiero@gmail.com>
2022-02-23 04:17:56 -0500

    fixed incoming data/donenv/minor things

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const port = process.env.PORT || 4000;
 const app = express();
-const URL = (process.env.URL)
+const URL = (process.env.URL) // assigning the variable from the dotenv file
 
 app.set("view engine", "ejs");
 
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 async function start() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(`${URL}`);
+  await page.goto(`${URL}`, {waitUntil: 'load', timeout: 0}); // Now pulls from the URL dotenv variable and does not time out at 30000 ms
 
   // episode Titles
   const episodesTitles = await page.evaluate(() => {
@@ -24,36 +24,26 @@ async function start() {
     );
   });
 
-  console.log(episodesTitles);
-  // const slicedTitles = episodesTitles.slice(2, 12);
-
-  //bot match ups total
-  const botMatchUps = await page.evaluate(() => {
-    return Array.from(
-      document.querySelectorAll(
-        " #mw-content-text > div > table:nth-child(n+37):nth-child(-n+64) a"
-      )
-    ).map((x) => x.textContent);
-  });
-  // console.log(botMatchUps);
+  //console.log(episodesTitles);
 
   // vs arr
   const botMatchRows = await page.evaluate(() => {
     return Array.from(
       document.querySelectorAll(
-        "#mw-content-text > div > table:nth-child(n+37):nth-child(-n+64) tr"
+        "table.article-table.fandom-table td" // This pulls the correct data from the tables. Took forever to realize removing the # sign was the reason it wasn't pulling the data correctly.
       )
-    ).map((x) => x.textContent);
+    ).map((x) => x.textContent)
   });
+  //console.log(botMatchRows)
 
   // slice match up rows arr
-  const size = 9;
+  const size = 8; // Now 8 since the Youtube Bonus Battle row is avoided
   const arrOfRows = [];
-  for (var i = 0; i <= botMatchRows.length; i += size) {
+  for (var i = 0; i <= botMatchRows.length - 1; i += size) { // wasn't starting at 0 the first time
     arrOfRows.push(botMatchRows.slice(i, i + size));
   }
-  console.log(arrOfRows[0][0]);
-  // console.log(arrOfRows);
+  //console.log(arrOfRows[0][0]);
+  //console.log(arrOfRows);
 
   // Server
   app.get("/", function (req, res) {
